3de9750ddae49f7385895e383ec6af25a722c750
Navneet Kumar <tech.navneetkumar@gmail.com>
2014-09-14 17:56:49 +0530

    All set for GRAPH integ

---
+++ src/com/example/fuzzylogicmodule/MyGraphViewData.java
@@ -0,0 +1,22 @@
+package com.example.fuzzylogicmodule;
+
+import com.jjoe64.graphview.GraphViewDataInterface;
+
+public class MyGraphViewData implements GraphViewDataInterface{
+
+	public MyGraphViewData() {
+		// TODO Auto-generated constructor stub
+	}
+	@Override
+	public double getX() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getY() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
